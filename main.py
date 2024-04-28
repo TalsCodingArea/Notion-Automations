b96464e03cf39667b620dd1edeb97f4d3282637f
TalsCodingArea <139641338+TalsCodingArea@users.noreply.github.com>
2024-04-28 22:08:46 +0300

    Added a 'Daily Success' method to measure and track my intended study hours to the actual time I've managed to be focus and learn

--- main.py
+++ main.py
@@ -30,6 +30,9 @@ def message(payload):
     if text.__contains__("spendings"):
         spendings_spent=str(getWeeklySum("Spendings 📦"))
         send_pushover_notification('SPENDINGS', "You've spent " + spendings_spent + " this week")
+    if text.__contains__("Daily Success"):
+        daily_success=str(int(getDailySuccess()*100))
+        send_pushover_notification('DAILY_SUCCESS', "You've achieved " + daily_success + "% of your daily goal")
     
 
 
@@ -76,6 +79,56 @@ def getWeeklySum(category):
         return(amount_sum)
     else:
         print("Error:", data_json)
+
+def getDailySuccess():
+    conn = http.client.HTTPSConnection("api.notion.com")
+    headers = {
+        'Authorization': 'Bearer ' + os.environ['NOTION_API_TOKEN'],
+        'Notion-Version': '2021-08-16',
+        'Content-Type': 'application/json'
+    }
+    today = (datetime.now()).isoformat()
+    intended_payload = {
+        'filter': {
+            'and': [
+                {
+                    'property': 'Date',
+                    'date': {
+                        'on_or_after': today,
+                    },
+                },
+            ],
+        },
+    }
+    reality_payload = {
+        'filter': {
+            'and': [
+                {
+                    'property': 'Date',
+                    'date': {
+                        'on_or_after': today,
+                    },
+                },
+            ],
+        },
+    }
+    conn.request('POST', f'/v1/databases/' + os.environ['DATABASE_ID'] + '/query', body=json.dumps(intended_payload), headers=headers)
+    res = conn.getresponse()
+    intended_data = res.read()
+    intended_data_json = json.loads(intended_data.decode('utf-8'))
+
+    conn.request('POST', f'/v1/databases/' + os.environ['DATABASE_ID'] + '/query', body=json.dumps(reality_payload), headers=headers)
+    res = conn.getresponse()
+    reality_data = res.read()
+    reality_data_json = json.loads(reality_data.decode('utf-8'))
+
+    if 'results' in intended_data_json and 'results' in reality_data_json:
+        intended_sum = sum(item['properties']['Amount']['number'] for item in intended_data_json['results'] if 'Amount' in item['properties'])
+        reality_sum = sum(item['properties']['Amount']['number'] for item in reality_data_json['results'] if 'Amount' in item['properties'])
+        result = float(reality_sum) / float(intended_sum)
+        return(result)
+    else:
+        print("Error:", intended_data_json, reality_data_json)
 
 def send_pushover_notification(subject, message):
     url = 'https://api.pushover.net/1/messages.json'
